2aaec48179ca32653275c3bd3fa11e7c82457fa2
marnvel <ricardom.velarde@gmail.com>
2023-10-16 10:18:51 -0300

    modularización. 16-10-2023. 10:18hs

---
+++ src/products/controllers/products.controller.ts
@@ -0,0 +1,16 @@
+import { Controller, Get } from '@nestjs/common';
+import { ProductsService } from '../services/products.service';
+
+@Controller('products')
+export class ProductsController {
+  constructor(private readonly: ProductsService) {}
+
+  @Get()
+  getHello(): string {
+    return this.productsService.getHello();
+  }
+  @Get('/hola')
+  getHello2(): string {
+    return this.productsService.getHola();
+  }
+}
