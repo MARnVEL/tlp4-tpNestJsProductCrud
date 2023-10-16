ba88a5ac83597d672ce4f93deaafb5abd869f310
marnvel <ricardom.velarde@gmail.com>
2023-10-16 11:17:23 -0300

    Instalacion config module. Configuración variables de entorno. Configuración cors. 16-10-2023. 11:17hs

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,19 @@
+
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [ProductsModule],
-  controllers: [AppController],
-  providers: [AppService]
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: `.${process.env.NODE_ENV}.env`,
+      isGlobal: true
+    }),
+    ProductsModule
+  ],
+  // controllers: [AppController],
+  // providers: [AppService]
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
+
+import * as morgan from "morgan";
+
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { CORS } from "./constants";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000, () => console.log(`Port: ${3000}`));
+  app.use(morgan('dev'));
+  const configService = app.get(ConfigService);
+  app.enableCors(CORS);
+  app.setGlobalPrefix('api');
+
+  await app.listen(
+    configService.get('PORT'),
+    () => console.log(
+      `Server running on port ${configService.get('PORT')}.
+      Go to http://localhost:${configService.get('PORT')}`)
+    );
 }
 bootstrap();
